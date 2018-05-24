aa651f17691fd6826e6e604d70a9eed355023f1c
Kok Seng <tankokseng@airasia.com>
2018-05-25 03:16:43 +0800

    add in docker image

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const app = require("./server");
 const express = require('express');
 
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
 app.use('/', express.static('public'))
 
-app.listen(3000);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
